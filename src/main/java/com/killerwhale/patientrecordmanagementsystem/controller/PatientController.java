afdf2d854a00084cbcbe4c7bf574a108fde44ada
killerwhale <alexandrisanas@gmail.com>
2022-10-14 16:39:19 +0300

    added get patient by id, added expection package

--- src/main/java/com/killerwhale/patientrecordmanagementsystem/controller/PatientController.java
+++ src/main/java/com/killerwhale/patientrecordmanagementsystem/controller/PatientController.java
@@ -1,12 +1,11 @@
 package com.killerwhale.patientrecordmanagementsystem.controller;
 
+import com.killerwhale.patientrecordmanagementsystem.exception.ResourceNotFoundException;
 import com.killerwhale.patientrecordmanagementsystem.model.Patient;
 import com.killerwhale.patientrecordmanagementsystem.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +21,13 @@ public class PatientController {
     public List<Patient> getAllPatients() {
         return patientRepository.findAll();
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Patient with id: %s does not exist", id)));
+
+        return ResponseEntity.ok(patient);
+    }
+
 }
